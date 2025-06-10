08b889ba89ef51f7b91f2fd588d8f17323e29788
JJayexd <kontakt.emilbj@gmail.com>
2025-06-10 12:45:18 +0200

    Tailwind Tailwind git commit -m Tailwind git push

---
+++ src/Components/ContentWrapper/ContentWrapper.jsx
@@ -0,0 +1,7 @@
+export const ContentWrapper = ({ children }) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { ContentWrapper } from "../ContentWrapper/ContentWrapper"
+
+export const Header = () => {
+    return (
+        <ContentWrapper class="container mx-auto">
+            <Header class="max-w-[1300px]">
+                <h1>TECHCOLLEGE</h1>
+            </Header>
+        </ContentWrapper>
+    )
+}
